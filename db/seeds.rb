0141b27b4b2a5378f825e474c3dc4d9fa3ea062f
Luanht <htluan2605@gmail.com>
2013-07-12 14:10:08 +0700

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,19 +18,16 @@ small_role = Role.create(:name => 'small', :maximum_packages => 3, :unit_amount 
 medium_role = Role.create(:name => 'medium', :maximum_packages => 10, :unit_amount => 10)
 large_role = Role.create(:name => 'large', :maximum_packages => 30, :unit_amount => 15)
 
-user = User.find_or_create_by_email :first_name => 'Thanh', :last_name => 'Luan', :email => 'htluan2605@gmail.com', :password => '123123', :password_confirmation => '123123', :frequency => 1000
+user = User.find_or_create_by_email :first_name => 'Thanh', :last_name => 'Luan', :email => 'admin@gmail.com', :password => '123123', :password_confirmation => '123123', :frequency => 1000
 user.add_role :admin
 
-user2 = User.find_or_create_by_email :first_name => 'Free', :last_name => 'User', :email => 'h.tluan2605@gmail.com', :password => '123123', :password_confirmation => '123123', :status => 'normal', :frequency => 1, :check_date_time => DateTime.now
+user2 = User.find_or_create_by_email :first_name => 'Free', :last_name => 'User', :email => 'free@gmail.com', :password => '123123', :password_confirmation => '123123', :status => 'normal', :frequency => 1, :check_date_time => DateTime.now
 user2.add_role :free
 
-user3 = User.find_or_create_by_email :first_name => 'Small', :last_name => 'User', :email => 'ht.luan2605@gmail.com', :password => '123123', :password_confirmation => '123123', :status => 'normal', :frequency => 2, :check_date_time => DateTime.now
+user3 = User.find_or_create_by_email :first_name => 'Small', :last_name => 'User', :email => 'small@gmail.com', :password => '123123', :password_confirmation => '123123', :status => 'normal', :frequency => 2, :check_date_time => DateTime.now
 user3.add_role :small
 
-user4 = User.find_or_create_by_email :first_name => 'Medium', :last_name => 'User', :email => 'htl.uan2605@gmail.com', :password => '123123', :password_confirmation => '123123', :status => 'normal', :frequency => 2, :check_date_time => DateTime.now
-user4.add_role :medium
-
-user5 = User.find_or_create_by_email :first_name => 'Large', :last_name => 'User', :email => 'htlu.an2605@gmail.com', :password => '123123', :password_confirmation => '123123', :status => 'normal', :frequency => 3, :check_date_time => DateTime.now
+user5 = User.find_or_create_by_email :first_name => 'Large', :last_name => 'User', :email => 'large@gmail.com', :password => '123123', :password_confirmation => '123123', :status => 'normal', :frequency => 3, :check_date_time => DateTime.now
 user5.add_role :large
 puts " Created users :#{user.name} #{user2.name}, #{user3.name}, #{user4.name},#{user5.name}"
 
