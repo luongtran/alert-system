14706a44edde8db4d51332b0c6ea714b8f06ced4
Luanht <htluan2605@gmail.com>
2013-07-12 15:01:40 +0700

    delete tmp

--- config/initializers/create_userdata_folder.rb
+++ config/initializers/create_userdata_folder.rb
@@ -1,4 +1,1 @@
-Dir.mkdir "#{Rails.root.to_s}/tmp"  unless File.directory? "#{Rails.root.to_s}/tmp"
-Dir.mkdir "#{Rails.root.to_s}/tmp/userdata"  unless File.directory? "#{Rails.root.to_s}/tmp/userdata"
-ENV['temp_folder_path'] = "#{Rails.root.to_s}/tmp/userdata/"
-ENV['s3_bucket_prefix'] = "send-switch-package"
+ENV['s3_bucket_prefix'] = "alert-system-package-"
