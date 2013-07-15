f06ef219de8a6e616b1f9bce2f1aef41daa4daed
Luanht <htluan2605@gmail.com>
2013-07-15 14:01:13 +0700

    fix mail sender

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ActionMailer::Base
-  default :from => "Send Switch Stuff <send-switch@example.com>"
+  default :from => "Alert System Stuff <alert-system@example.com>"
 
   def expire_email(user)
     mail(:to => user.email, :subject => "Subscription Cancelled")
   end
-  
+
   def validate_email(user)
     @user = user
     mail(:to => @user.email, :subject => "Are you still alive ?")
