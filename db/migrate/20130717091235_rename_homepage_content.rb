35f66c3ca7afb229f501c88d4ce6d482b675acdb
Luanht <htluan2605@gmail.com>
2013-07-17 17:13:52 +0700

    fa

--- db/migrate/20130717091235_rename_homepage_content.rb
+++ db/migrate/20130717091235_rename_homepage_content.rb
@@ -1,8 +1,8 @@
 class RenameHomepageContent < ActiveRecord::Migration
   def up
-    rename_column :home_page_contents, :pagefoot_inforunit2_head_content, :pagefoot_inforunit2_content
-    rename_column :home_page_contents, :pagefoot_inforunit3_head_content, :pagefoot_inforunit3_content
-    rename_column :home_page_contents, :pagefoot_inforunit4_head_content, :pagefoot_inforunit4_content
+    #rename_column :home_page_contents, :pagefoot_inforunit2_head_content, :pagefoot_inforunit2_content
+    #rename_column :home_page_contents, :pagefoot_inforunit3_head_content, :pagefoot_inforunit3_content
+    #rename_column :home_page_contents, :pagefoot_inforunit4_head_content, :pagefoot_inforunit4_content
   end
 
   def down
