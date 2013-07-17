3cab2e1fb1de08fe55fd4b89f10923a6cdae65db
Luanht <htluan2605@gmail.com>
2013-07-17 17:56:09 +0700

    last migrate

--- db/schema.rb
+++ db/schema.rb
@@ -54,29 +54,6 @@ ActiveRecord::Schema.define(:version => 20130717084128) do
     t.datetime "updated_at",   :null => false
   end
 
-  create_table "home_page_contents", :force => true do |t|
-    t.string  "name"
-    t.string  "title"
-    t.text    "notepad_notetext"
-    t.string  "introduce_lead"
-    t.text    "introduce_inf_1"
-    t.text    "introduce_inf_2"
-    t.text    "introduce_inf_3"
-    t.string  "signupquick_lead"
-    t.text    "notice"
-    t.string  "halfpage_lead"
-    t.text    "halfpage_content"
-    t.string  "pagefoot_inforunit1_head"
-    t.text    "pagefoot_inforunit1_content"
-    t.string  "pagefoot_inforunit2_head"
-    t.text    "pagefoot_inforunit2_content"
-    t.string  "pagefoot_inforunit3_head"
-    t.text    "pagefoot_inforunit3_content"
-    t.string  "pagefoot_inforunit4_head"
-    t.text    "pagefoot_inforunit4_content"
-    t.boolean "active",                      :default => false
-  end
-
   create_table "item_types", :force => true do |t|
     t.string "name"
     t.string "description"
